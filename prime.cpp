f0285a07743115a7aea2cf82f76ef3a43774666d
exiledkingcc <exiledkingcc@gmail.com>
2016-09-17 21:42:20 +0800

    add prime_numbers_test(BigUint& b)

---
+++ prime.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include "prime.h"
+
+namespace simple_rsa {
+
+void primer_numbers_test(BigUint& b) {
+  if (b.is_even()) {
+    b += 1;
+  }
+  std::vector<uint32_t> remainders(PRIME_NUMBERS_SIZE);
+  for (int i = 0; i < PRIME_NUMBERS_SIZE; ++i) {
+    remainders[i] = b % PRIME_NUMBERS[i];
+  }
+  bool ok = true;
+  do {
+    ok = true;
+    for (int i = 0; i < PRIME_NUMBERS_SIZE; ++i) {
+      if (remainders[i] == 0) {
+        ok = false;
+      }
+    }
+    if (!ok) {
+      b += 2;
+      for (int i = 0; i < PRIME_NUMBERS_SIZE; ++i) {
+        remainders[i] = (remainders[i] + 2) % PRIME_NUMBERS[i];
+      }
+      continue;
+    }
+  } while (!ok);
+}
+
+} // namespace simple_rsa
--- prime.h
+++ prime.h
@@ -1,8 +1,17 @@
+#ifndef _PRIME_H__
+#define _PRIME_H__ 1
+
 #include <cstdint>
+#include "biguint.h"
 
 namespace simple_rsa {
 
 extern const uint32_t PRIME_NUMBERS[];
 extern const int PRIME_NUMBERS_SIZE;
 
+// make b and all PRIME_NUMBERS are relatively prime
+void primer_numbers_test(BigUint& b);
+
 } // namespace simple_rsa
+
+#endif // _PRIME_H__
