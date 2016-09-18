933138bab1dbcf3dc496609150c7a0eeb6e8bbb8
exiledkingcc <exiledkingcc@gmail.com>
2016-09-18 22:56:33 +0800

    add miller_rabin_test declare

--- prime.h
+++ prime.h
@@ -12,6 +12,8 @@ extern const int PRIME_NUMBERS_SIZE;
 // make b and all PRIME_NUMBERS are relatively prime
 void primer_numbers_test(BigUint& b);
 
+bool miller_rabin_test(const BigUint& b);
+
 } // namespace simple_rsa
 
 #endif // _PRIME_H__
